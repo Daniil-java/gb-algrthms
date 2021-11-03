38f8fc4db34cd69fb91059ad3e93e8f36724150b
Daniil <saponembigas@gmail.com>
2021-11-03 16:20:49 +0300

    -1

---
+++ src/main/java/homeworks/firth/TwoSideLinkedList.java
@@ -0,0 +1,6 @@
+package homeworks.firth;
+
+public interface TwoSideLinkedList<E>  extends LinkedList<E> {
+    void insertLast(E value);
+    E getLast();
+}
---
+++ src/main/java/homeworks/firth/TwoSideLinkedListImpl.java
@@ -0,0 +1,105 @@
+package homeworks.firth;
+
+public class TwoSideLinkedListImpl<E> extends SimpleLinkedListImpl<E> implements TwoSideLinkedList<E> {
+
+    protected Node<E> last;
+
+    @Override
+    public void insertFirst(E value) {
+        super.insertFirst(value);
+        if (size == 1) {
+            last = first;
+        }
+    }
+
+    @Override
+    public void insertLast(E value) {
+        Node<E> newNode = new Node<>(value, null);
+
+        if (isEmpty()) {
+            insertFirst(value);
+            return;
+        }
+
+        last.next = newNode;
+        last = newNode;
+        size++;
+    }
+
+    @Override
+    public E removeFirst() {
+        E removedValue = super.removeFirst();
+
+        if (isEmpty()) {
+            last = null;
+        }
+
+
+        return removedValue;
+    }
+
+
+    public E removeLast() {
+//        E removedValue = super.remove(last.item);
+//
+//        if (isEmpty()) {
+//            last = null;
+//        }
+//        size--;
+//        return removedValue;
+
+        Node<E> current = first;
+        Node<E> prev = null;
+
+        while (current != null) {
+            if (current.item.equals(last.item)) {
+                break;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+        prev.next = current.next;
+
+        current.next = null;
+        size--;
+
+        return (E) prev;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        Node<E> current = first;
+        Node<E> prev = null;
+
+        while (current != null) {
+            if (current.item.equals(value)) {
+                break;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            return false;
+        } else if (current == first) {
+            removeFirst();
+            return true;
+        } else if(current == last) {
+            last = prev;
+            last.next = null;
+        }
+        prev.next = current.next;
+
+        current.next = null;
+        size--;
+
+
+        return true;
+    }
+
+    @Override
+    public E getLast() {
+        return last.item;
+    }
+}
